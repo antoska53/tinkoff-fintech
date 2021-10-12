47a1a9da80e2933fb3554a4c9533e276a219a723
tosha <tosha170@outlook.com>
2021-10-12 19:31:46 +0300

    fix
    исправление замечаний, обращение к вьюхам в main activity

--- TinkoffMessenger/app/src/main/java/ru/myacademyhomework/tinkoffmessenger/MainActivity.kt
+++ TinkoffMessenger/app/src/main/java/ru/myacademyhomework/tinkoffmessenger/MainActivity.kt
@@ -2,24 +2,36 @@ package ru.myacademyhomework.tinkoffmessenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import ru.myacademyhomework.tinkoffmessenger.MessageView.EmojiView
+import ru.myacademyhomework.tinkoffmessenger.MessageView.FlexBoxLayout
+import ru.myacademyhomework.tinkoffmessenger.MessageView.MessageViewGroup
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val emojiView = findViewById<EmojiView>(R.id.emoji_view)
-        emojiView.setOnClickListener {
-            emojiView.isSelected = !it.isSelected
-            if(emojiView.isSelected){
-                emojiView.textCount = "2"
-                emojiView.smile = "\uD83D\uDE06"
-            }else{
-                emojiView.textCount = "1"
-                emojiView.smile = "\uD83D\uDE05"
+        val messageViewGroup = findViewById<MessageViewGroup>(R.id.message_view_group)
+        for (i in 0 until messageViewGroup.childCount) {
+            val child = messageViewGroup.getChildAt(i)
+            if (child is FlexBoxLayout) {
+                for (j in 0 until child.childCount) {
+                    val childEmoji = child.getChildAt(j)
+                    if (childEmoji is EmojiView) {
+                        childEmoji.setOnClickListener {
+                            childEmoji.isSelected = !it.isSelected
+                            if (childEmoji.isSelected) {
+                                childEmoji.textCount = "2"
+                                childEmoji.smile = "\uD83D\uDE06"
+                            } else {
+                                childEmoji.textCount = "1"
+                                childEmoji.smile = "\uD83D\uDE05"
+                            }
+                        }
+                    }
+                }
             }
         }
-
     }
 }
