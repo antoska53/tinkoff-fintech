cf90df7925aafa1278a4111d29d942ae7c1bc8d5
Antoska <tosha170@outlook.com>
2023-08-08 19:47:20 +0300

    refactoring chatViewHolder
    почистил код

--- TinkoffMessenger/app/src/main/java/ru/myacademyhomework/tinkoffmessenger/presentation/chatfragment/ChatViewHolder.kt
+++ TinkoffMessenger/app/src/main/java/ru/myacademyhomework/tinkoffmessenger/presentation/chatfragment/ChatViewHolder.kt
@@ -1,6 +1,5 @@
 package ru.myacademyhomework.tinkoffmessenger.presentation.chatfragment
 
-import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
